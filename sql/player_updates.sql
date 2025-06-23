e0c207fee2778938997bdea251eacccb3dd744ea
shyamdr <shyamdrangapure@gmail.com>
2025-06-23 09:48:47 +0530

    Archiving dashboard.py

---
+++ sql/player_updates.sql
@@ -0,0 +1,9 @@
+update players
+set
+	full_name = 'Eshan Malinga'
+	first_last_name = 'Eshan Malinga'
+	batting_hand = 'Left-hand bat'
+	date_of_birth = '2001-02-04'
+	bowling_style = 'RFM/RAFM'
+where key_cricinfo = '1306214';
+
