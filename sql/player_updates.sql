6ac7936013a515ccb1675d1ef2d711d302105603
shyamdr <shyamdrangapure@gmail.com>
2025-06-25 16:10:16 +0530

    Manually fix incorrectly mapped player information

--- sql/player_updates.sql
+++ sql/player_updates.sql
@@ -1,9 +1,306 @@
-update players
-set
-	full_name = 'Eshan Malinga'
-	first_last_name = 'Eshan Malinga'
-	batting_hand = 'Left-hand bat'
-	date_of_birth = '2001-02-04'
-	bowling_style = 'RFM/RAFM'
-where key_cricinfo = '1306214';
+UPDATE players
+SET
+    full_name = 'Eshan Malinga',
+    first_last_name = 'Eshan Malinga',
+    batting_hand = 'Left-hand bat',
+    date_of_birth = '2001-02-04',
+    bowling_style = 'RFM/RAFM'
+WHERE key_cricinfo = '1306214';
 
+UPDATE players
+SET
+    full_name = 'Mohit Mahipal Sharma',
+    first_last_name = 'Mohit Sharma',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1988-09-18',
+    bowling_style = 'RM/RAM',
+    country = 'India'
+WHERE key_cricinfo = '537119';
+
+UPDATE players
+SET
+    full_name = 'Halhadar Das',
+    first_last_name = 'Halhadar Das',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'N/A',
+    player_role = 'Wicket Keeper',
+    date_of_birth = '1986-12-10',
+    country = 'India',
+    bowling_style = 'N/A'
+WHERE key_cricinfo = '236810';
+
+UPDATE players
+SET
+    full_name = 'Arindam Nanigopal Ghosh',
+    first_last_name = 'Arindam Ghosh',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Batsman',
+    date_of_birth = '1986-10-19',
+    country = 'India',
+    bowling_style = 'RM/RAM'
+WHERE key_cricinfo = '220435';
+
+UPDATE players
+SET
+    full_name = 'Spencer Henry Johnson',
+    first_last_name = 'Spencer Johnson',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Left-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1995-12-16',
+    country = 'Australia',
+    bowling_style = 'LF/LAF'
+WHERE key_cricinfo = '1123718';
+
+UPDATE players
+SET
+    full_name = 'Vipraj Nigam',
+    first_last_name = 'Vipraj Nigam',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '2004-07-28',
+    country = 'India',
+    bowling_style = 'LB'
+WHERE key_cricinfo = '1449074';
+
+UPDATE players
+SET
+    full_name = 'Brett Geeves',
+    first_last_name = 'Brett Geeves',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1982-06-13',
+    country = 'Australia',
+    bowling_style = 'RFM/RAFM'
+WHERE key_cricinfo = '5472';
+
+UPDATE players
+SET
+    full_name = 'Congenige Randhi Dilhara Fernando',
+    first_last_name = 'Dilhara Fernando',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1982-06-13',
+    country = 'Sri Lanka',
+    bowling_style = 'RFM/RAFM'
+WHERE key_cricinfo = '48835';
+
+UPDATE players
+SET
+    first_last_name = 'Ajantha Mendis'
+WHERE key_cricinfo = '268739';
+
+UPDATE players
+SET
+    first_last_name = 'Jeevan Mendis',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Right-arm',
+    bowling_style = 'LB',
+    date_of_birth = '1983-01-15'
+WHERE key_cricinfo = '49700';
+
+UPDATE players
+SET
+    full_name = 'Magina Thilan Thushara Mirando',
+    first_last_name = 'Thilan Thushara',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Left-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1981-03-01',
+    country = 'Sri Lanka',
+    bowling_style = 'LFM/LAFM'
+WHERE key_cricinfo = '49677';
+
+UPDATE players
+SET
+    date_of_birth = '1989-04-03'
+WHERE key_cricinfo = '233514';
+
+UPDATE players
+SET
+    full_name = 'Rajesh Bishnoi',
+    first_last_name = 'Rajesh Bishnoi',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Batsman',
+    date_of_birth = '1987-10-08',
+    country = 'India',
+    bowling_style = 'RMF/RAMF'
+WHERE key_cricinfo = '236766';
+
+UPDATE players
+SET
+    full_name = 'Asad Ullah Khan Pathan',
+    first_last_name = 'Asad Pathan',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1984-06-17',
+    country = 'India',
+    bowling_style = 'RM/RAM'
+WHERE key_cricinfo = '430401';
+
+UPDATE players
+SET
+    full_name = 'Harpreet Singh Bhatia',
+    first_last_name = 'Harpreet Bhatia',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Batsman',
+    date_of_birth = '1991-08-11',
+    country = 'India',
+    bowling_style = 'RM/RAM'
+WHERE key_cricinfo = '340854';
+
+UPDATE players
+SET
+    full_name = 'Mohammed Arshad Khan',
+    first_last_name = 'Arshad Khan',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Left-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1997-12-20',
+    country = 'India',
+    bowling_style = 'LFM/LAFM'
+WHERE key_cricinfo = '1244751';
+
+UPDATE players
+SET
+    full_name = 'Nathu Bharat Singh',
+    first_last_name = 'Nathu Singh',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1995-09-08',
+    country = 'India',
+    bowling_style = 'LFM/LAFM'
+WHERE key_cricinfo = '853259';
+
+UPDATE players
+SET
+    full_name = 'Shubham Sanjay Agarwal',
+    first_last_name = 'Shubham Agarwal',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'All-rounder',
+    date_of_birth = '1993-11-21',
+    country = 'India',
+    bowling_style = 'LBG'
+WHERE key_cricinfo = '1079668';
+
+UPDATE players
+SET
+    full_name = 'Rahul Sharma',
+    first_last_name = 'Rahul Sharma',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1986-11-30',
+    country = 'India',
+    bowling_style = 'LBG'
+WHERE key_cricinfo = '272994';
+
+UPDATE players
+SET
+    full_name = 'Rishi Dhawan',
+    first_last_name = 'Rishi Dhawan',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'All-rounder',
+    date_of_birth = '1990-02-19',
+    country = 'India',
+    bowling_style = 'RMF/RAMF'
+WHERE key_cricinfo = '290727';
+
+UPDATE players
+SET
+    full_name = 'Abu Nechim Ahmed',
+    first_last_name = 'Abu Nechim',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1988-11-05',
+    country = 'India',
+    bowling_style = 'RMF/RAMF'
+WHERE key_cricinfo = '228430';
+
+UPDATE players
+SET
+    full_name = 'Anukul Sudhakar Roy',
+    first_last_name = 'Anukul Roy',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Left-arm',
+    player_role = 'All-rounder',
+    date_of_birth = '1998-11-30',
+    country = 'India',
+    bowling_style = 'SLA'
+WHERE key_cricinfo = '1079839';
+
+UPDATE players
+SET
+    full_name = 'Pasqual Handi Kamindu Dilanka Mendis',
+    first_last_name = 'Kamindu Mendis',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Both',
+    player_role = 'All-rounder',
+    date_of_birth = '1998-09-30',
+    country = 'Sri Lanka',
+    bowling_style = 'OB,SLA'
+WHERE key_cricinfo = '784373';
+
+UPDATE players
+SET
+    full_name = 'Darren Michael Bravo',
+    first_last_name = 'Darren Bravo',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'Batsman',
+    date_of_birth = '1989-02-06',
+    country = 'West Indies',
+    bowling_style = 'RM/RAM'
+WHERE key_cricinfo = '277472';
+
+UPDATE players
+SET
+    full_name = 'Demuni Nuwan Tharanga Zoysa',
+    first_last_name = 'Nuwan Zoysa',
+    batting_hand = 'Left-hand bat',
+    bowling_hand = 'Left-arm',
+    player_role = 'Bowler',
+    date_of_birth = '1978-05-13',
+    country = 'Sri Lanka',
+    bowling_style = 'LFM/LAFM'
+WHERE key_cricinfo = '51050';
+
+UPDATE players
+SET
+    full_name = 'Anshul Kamboj',
+    first_last_name = 'Anshul Kamboj',
+    batting_hand = 'Right-hand bat',
+    bowling_hand = 'Right-arm',
+    player_role = 'All-rounder',
+    date_of_birth = '2000-12-06',
+    country = 'India',
+    bowling_style = 'RM/RAM'
+WHERE key_cricinfo = '1175428';
+
+/*
+1175428
+1452455
+236694
+1209126
+7547
+594322
+30719
+528985
+6513
+1460529
+1194959
+38699
+*/
